7ef31c52dfb54fe21037f207579293166263c11e
Max Nicolai <maxvnicolai@gmail.com>
2020-08-29 21:01:46 -0700

    html routes tested/working running from server file, troubleshooting integration from seperate file

--- public/assets/js/routes.js
+++ public/assets/js/routes.js
@@ -1,3 +1,21 @@
+//Required node modules
+const path = require('path')
+
+//HTML ROUTES
+
+//Exports routes, uses fn to pass ref to app as param
+module.exports = function(app) {
+  //Homepage route
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../index.html'));
+  });
+
+  //Note page route
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../notes.html'));
+  });
+}
+//API ROUTES
 //GET method to be called with getNote fn
 //POST method to be called with saveNote fn
 //DELETE method to be called with deleteNote fn
--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,14 +2,33 @@
 const express = require('express')
 const path = require('path')
 
-//Shorthand for calling express methods
+//Establishes express server
 const app = express()
+
+//Employs express to handle URLS and JSON files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Finds port in deployed environment, or sets for local hosting
 const PORT = process.env.PORT || 8080
 
-//Test Check
-app.listen(PORT, (err) => {
+//Connects server code to route files
+//require('./routes')
+
+//Establishes server, reports back
+app.listen(PORT, err => {
   if (err) throw err;
-  console.log(`testing on ${PORT}`)
-})
+  console.log(`listening ${PORT}`);
+});
+
+
+  //Homepage
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../index.html'));
+    console.log('hello')
+  });
+
+  //Note page route
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../notes.html'));
+  });
